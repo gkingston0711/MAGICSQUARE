012bc19a49af3f8deeaa86b76c23c90de2e5e66e
Joey Kingston <48415221+gkingston0711@users.noreply.github.com>
2020-08-31 11:55:28 -0700

    second push

--- venv/bin/one.py
+++ venv/bin/one.py
@@ -2,10 +2,21 @@
 def openCSV(fileName):
     FileOpen = open(fileName,'r')
 
+
+
     for line in FileOpen:
+
         line = line.rstrip('\n')
         line = line.strip(' ')
         line = line.split()
+
+        Length = len(line)
+        if Length == 1:
+            arr = []
+
+
+
+
 
         print line
 
